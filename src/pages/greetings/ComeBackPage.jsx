5d1189de108a2c63223c9481625ee95cbe3c5fa3
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-07-03 02:02:06 +0600

    designed email verification page and it's components, added a modal window

---
+++ src/pages/greetings/ComeBackPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from "../../components/greetings/Greeting";
+
+const ComeBackPage = () => {
+    return (
+        <>
+            <Greeting message={'С возвращением!'}/>
+        </>
+    )
+}
+
+export default ComeBackPage;
