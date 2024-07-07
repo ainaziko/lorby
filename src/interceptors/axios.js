f8d799bd0d9e6a3c2df6b12ff779d7afda52f7b6
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-07-07 17:25:07 +0600

    optimized the process of token refreshment, made sure it works once instead of constant work

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -2,14 +2,18 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://auth-project-production.up.railway.app/api/auth';
 
+const refresh = false; 
+
 axios.interceptors.response.use (resp => resp, async error => {
-     if(error.response.status === 401) {
+    if(error.response.status === 401 && !refresh) {
+        refresh = true; 
         const response = await axios.post('refresh', {}, { withCredentials:  true });
 
         if(response.status === 200) {
             axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
             return axios(error.config);
         }
-     }
-     return error;
+    }
+    refresh = false;
+    return error;
 })
