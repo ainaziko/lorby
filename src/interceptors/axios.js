d644211f82c24f79bba099c300405e6a3b27816e
Ainazik Momunalieva <58284183+ainaziko@users.noreply.github.com>
2024-07-07 18:03:54 +0600

    updated babel version

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://auth-project-production.up.railway.app/api/auth';
 
-const refresh = false; 
+let refresh = false; 
 
 axios.interceptors.response.use (resp => resp, async error => {
     if(error.response.status === 401 && !refresh) {
